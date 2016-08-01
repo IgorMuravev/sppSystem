80655647cd89549020eeb6109b84673f6897ff45
Igor Muravev <to.imuravev@gmail.com>
2016-08-01 15:52:08 +0500

    +first tool Graph Gen

---
+++ ImuravevSoft.Core/Tool/BaseTool.cs
@@ -0,0 +1,22 @@
+﻿using ImuravevSoft.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImuravevSoft.Core.Tool
+{
+    public abstract class BaseTool:UserControl
+    {
+        private readonly List<BaseData> usedData = new List<BaseData>();
+        public List<BaseData> UsedData
+        {
+            get
+            {
+                return usedData;
+            }
+        }
+
+    }
+}
